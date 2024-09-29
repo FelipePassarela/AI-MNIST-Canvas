ccb62bf988e48b7dfa31251203f06b03afc6889f
FelipePassarela <126916744+FelipePassarela@users.noreply.github.com>
2024-09-29 19:06:54 -0300

    Optimize paint and draw methods in CanvasDisplay

--- utils/canvas_display.py
+++ utils/canvas_display.py
@@ -15,31 +15,35 @@ class CanvasDisplay:
         self.grid.fill(0)
 
     def paint(self, x, y, brush_size=1.8, max_intensity=1.0):
-        y_indices, x_indices = np.indices(self.grid.shape)
-        distances = np.sqrt((x_indices - x)**2 + (y_indices - y)**2)
+        min_x = max(int(x - brush_size), 0)
+        max_x = min(int(x + brush_size), self.width - 1)
+        min_y = max(int(y - brush_size), 0)
+        max_y = min(int(y + brush_size), self.height - 1)
+
+        if min_x > max_x or min_y > max_y:
+            return
+
+        y_indices, x_indices = np.indices((max_y - min_y + 1, max_x - min_x + 1))
+        y_indices += min_y
+        x_indices += min_x
+
+        distances = np.sqrt((x_indices - x) ** 2 + (y_indices - y) ** 2)
         mask = distances <= brush_size
-        intensities = max_intensity * (1 - distances / brush_size)
-        self.grid[mask] = np.minimum(self.grid[mask] + intensities[mask], max_intensity)
+        intensity = max_intensity * (1 - distances / brush_size)
+
+        self.grid[min_y:max_y + 1, min_x:max_x + 1][mask] = np.minimum(
+            self.grid[min_y:max_y + 1, min_x:max_x + 1][mask] + intensity[mask], 1.0
+        )
 
     def draw(self, screen, x, y):
-        canvas_rect = pg.Rect(
-            x, y, 
-            self.width * self.cell_scale, 
-            self.height * self.cell_scale
-        )
-        screen.fill(self.bg_color, canvas_rect)
+        mask = self.grid > 0
+        color_array = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+        color_array[mask] = (self.grid[mask][:, None] * self.brush_color).astype(np.uint8)
+        color_array = np.transpose(color_array, (1, 0, 2))
 
-        for i in range(self.height):
-            for j in range(self.width):
-                if self.grid[i, j] == 0:
-                    continue
-
-                color = [c * self.grid[i, j] for c in self.brush_color]
-                cell_rect = pg.Rect(
-                    j * self.cell_scale, 
-                    i * self.cell_scale, 
-                    self.cell_scale, self.cell_scale)
-                pg.draw.rect(screen, color, cell_rect)
+        surf = pg.surfarray.make_surface(color_array)
+        scaled_surf = pg.transform.scale(surf, (self.width * self.cell_scale, self.height * self.cell_scale))
+        screen.blit(scaled_surf, (x, y))
 
     def print(self):
         for row in self.grid:
